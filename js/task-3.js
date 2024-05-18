b55e2e8d7bb1970fdfef139317ae1db814f97712
Yevhen Nesvit <yevhen.nesvit@gmail.com>
2024-05-18 16:53:18 +0300

    initial commit

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+function getElementWidth(content, padding, border) {
+    
+    let contentWidth = parseFloat(content);
+    let paddingValue = parseFloat(padding);
+    let borderValue = parseFloat(border);
+    
+    let totalWidth = contentWidth + 2 * paddingValue + 2 * borderValue;
+    
+    return totalWidth;
+}
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
